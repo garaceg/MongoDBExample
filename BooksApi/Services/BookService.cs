4626aa3e9d48c76c388e360d8707076a5b46a59e
ggarcia <ggarcia@fcebenefit.com>
2021-07-22 16:56:28 -0500

    Add project files.

---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApi.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() => _books.Find(book => true).ToList();
+        public Book Get(string id) => _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+    }
+}
